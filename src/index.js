9c6125ff8a80f77322c2b79399997187e16bf031
Mohammed Nayeem <nayeem.gmit@gmail.com>
2021-07-12 19:52:03 +0530

    commit on master

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+// import App from "./App";
+// import App from "./01_RenderProps/App";
+// import App from "./02_Context/App";
+// import App from "./03_useState/App";
+// import App from "./04_FetchData/App";
+// import App from "./05_useReducer/App";
+import App from "./06_useCallback/App";
+// import App from "./Assignment05/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

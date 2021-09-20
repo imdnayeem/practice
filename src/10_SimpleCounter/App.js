4c1b6aed8787f116b2bd9a569c61a964bee31ad6
Mohammed Nayeem <nayeem.gmit@gmail.com>
2021-09-20 14:42:47 +0530

    first commit

--- src/10_SimpleCounter/App.js
+++ src/10_SimpleCounter/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
   const [userData, setUserData] = useState([]);
 
   const incrementCount = () => {
@@ -12,14 +12,29 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://randomuser.me/api")
-      .then((res) => setUserData(JSON.stringify(res.data, null, 2)))
+      // .get(`https://randomuser.me/api?page=${count}`)
+      .get(`https://randomuser.me/api?results=${count}`)
+      .then((res) => {
+        if (res === undefined) return;
+        const newData = [
+          ...userData,
+          res.data.results.map((res, id) => (
+            <div key={id}>
+              <pre>
+                {res.name.last} {res.name.first}
+              </pre>
+              <img src={res.picture.thumbnail} alt="thumbnail" />
+            </div>
+          )),
+        ];
+        setUserData(newData);
+      })
       .catch((err) => console.log(err));
-  }, []);
+  }, [count]);
 
   return (
     <div className="App">
-      <button onClick={incrementCount}>{count}</button>
+      <button onClick={incrementCount}>next 2 users</button>
       <pre>{userData}</pre>
     </div>
   );
